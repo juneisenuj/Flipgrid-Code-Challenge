9a1b321bc7a8567e587d813fad29d672b9c25976
juneisenuj <junehaklee94@gmail.com>
2022-02-27 14:25:59 -0600

    added classnames and states for signup information form

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,27 +2,32 @@ import React, { useState } from 'react';
 import './styles/Form/form.scss';
 
 const Form = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className='form-container'>
-            <form>
-                <div>form intro</div>
+            <form className='form'>
+                <div className='form-intro'>form intro</div>
 
-                <div>
-                    <label>
+                <div className='row'>
+                    <label className='label'>
                         First Name
                     </label>
                     <input />
                 </div>
 
-                <div>
-                    <label>
+                <div className='row'>
+                    <label className='label'>
                         Email Address
                     </label>
                     <input />
                 </div>
 
-                <div>
-                    <label>
+                <div className='row'>
+                    <label className='label'>
                         Password
                     </label>
                     <input />
