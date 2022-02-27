f4527f3bcb96275c6ddf55ec615a1e60828e46e8
juneisenuj <junehaklee94@gmail.com>
2022-02-27 14:16:11 -0600

    added a simple form

---
+++ src/Form.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './styles/Form/form.scss';
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <div>form intro</div>
+
+                <div>
+                    <label>
+                        First Name
+                    </label>
+                    <input />
+                </div>
+
+                <div>
+                    <label>
+                        Email Address
+                    </label>
+                    <input />
+                </div>
+
+                <div>
+                    <label>
+                        Password
+                    </label>
+                    <input />
+                </div>
+                <button>button</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Form from './Form';
 
 const Main = () => {
     return (
         <div className='main'>
-            Main
+            <Form />
         </div>
     );
 };
